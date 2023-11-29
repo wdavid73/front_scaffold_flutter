5ba3efbea719a065225e1594134b97bf494326e9
Wilson Padilla <wdavidw26@gmail.com>
2023-11-29 15:31:16 -0500

    :alien: api configuration

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -20,7 +20,6 @@ class _MyHomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Front Scaffold Flutter'),
       ),
       body: Center(
