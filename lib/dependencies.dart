85d2764ef4e300ecbe0b6a666fc8ee071270e1b6
Wilson Padilla <wdavidw26@gmail.com>
2023-12-21 14:24:17 -0500

    :ambulance: fix wrong implementation of repository impl in RepositoryProvider

--- lib/dependencies.dart
+++ lib/dependencies.dart
@@ -3,11 +3,12 @@ import 'package:front_scaffold_flutter/domain/repository/example_repository.dart
 import 'package:front_scaffold_flutter/domain/usecase/example_usecase.dart';
 import 'package:front_scaffold_flutter/ui/bloc/bloc_imports.dart';
 import 'package:front_scaffold_flutter/ui/bloc/example_bloc/example_bloc.dart';
+import 'package:front_scaffold_flutter/data/api/api_client.dart';
 
 List<RepositoryProvider> buildRepositories() {
   return [
     RepositoryProvider<ExampleRepository>(
-      create: (context) => ExampleRepositoryImpl(context.read()),
+      create: (context) => ExampleRepositoryImpl(ApiClient.instance),
     ),
     RepositoryProvider<ExampleUseCase>(
       create: (context) => ExampleUseCase(context.read()),
